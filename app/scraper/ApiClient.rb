3ca480ab154d5bd9003530b7f67c22f86963f727
Nerbyk <danil.maximov2000@gmail.com>
2020-10-19 12:27:35 -0400

    user agents varaint

--- app/scraper/ApiClient.rb
+++ app/scraper/ApiClient.rb
@@ -6,11 +6,16 @@ require 'useragent'
 
 class ApiParserClient
   def self.parse_page(url)
-    user_agent_desktop = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '\
-      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 '\
-      'Safari/537.36' 
-    
-    headers = { 'User-Agent': user_agent_desktop}
+    user_agent_desktop = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36', 
+                          'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36', 
+                          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36', 
+                          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36', 
+                          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 
+                          'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36', 
+                          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36', 
+                          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36', 
+                          'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36']
+    headers = { 'User-Agent': user_agent_desktop.sample}
     page = HTTParty.get(url, :headers => headers)
     Nokogiri::HTML(page)
   end
@@ -19,3 +24,5 @@ class ApiParserClient
 
   attr_reader :url
 end
+
+ApiParserClient.parse_page('text')
